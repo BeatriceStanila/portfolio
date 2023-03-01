f357673de51bb4c71cb111ecb8598c800cc1484a
btxoana <beatrice.stanila@gmail.com>
2023-03-01 12:12:28 +0000

    Contact page added

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero/Hero";
 import Projects from "@/components/Projects/Projects";
 import About from "@/components/About/About";
 import Skills from "@/components/Skills/Skills";
+import Contact from "@/components/Contact/Contact";
 
 export default function Home() {
   return (
@@ -43,6 +44,7 @@ export default function Home() {
         </div>
 
         <Skills />
+        <Contact />
       </main>
     </>
   );

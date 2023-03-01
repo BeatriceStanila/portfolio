db29c55624112553e2a7e2c06b96daac91330208
btxoana <beatrice.stanila@gmail.com>
2023-03-01 14:46:22 +0000

    Css modules all done

--- my-website/src/components/Hero/Hero.js
+++ my-website/src/components/Hero/Hero.js
@@ -1,22 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import styles from "./hero.module.css";
 import Navbar from "../Navbar/Navbar";
 
 export default function Hero({ greeting, name, intro }) {
   return (
-    <div className="main">
-      <div className="hero">
+    <div className={styles.main}>
+      <div className={styles.hero}>
         <Navbar />
-        <div className="hero-content">
+        <div className={styles.heroContent}>
           <div>
             <p>{greeting}</p>
             <h1>{name}</h1>
             <h2>{intro}</h2>
-            <div className="hero-btn">
-              <Link href="#!" class="btn" role="button">
+            <div className={styles.heroButtons}>
+              <Link href="/#projects" class={styles.btn} role="button">
                 Projects
               </Link>
-              <Link href="#!" class="btn btn-bold" role="button">
+              <Link
+                href="./BeatriceStanilaResume.pdf"
+                class={styles.btnBold}
+                role="button"
+              >
                 Resume
               </Link>
             </div>
--- my-website/src/components/Skills/Skills.js
+++ my-website/src/components/Skills/Skills.js
@@ -1,14 +1,15 @@
 import React from "react";
+import styles from "./skills.module.css";
 
 function Skills() {
   return (
-    <div id="skills" className="skills">
-      <div>
-        <h1>My skills</h1>
-        <div className="skills-container">
-          <div className="skills-frontend">
-            <img src="/data.svg" className="svg" />
-            <ul>
+    <div id="skills" className={styles.skills}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.header}>My skills</h1>
+        <div className={styles.container}>
+          <div className={styles.frontend}>
+            <img src="/data.svg" className={styles.svg} />
+            <ul className={styles.list}>
               <li>JavaScript</li>
               <li>HTML & CSS</li>
               <li>React & Next</li>
@@ -17,8 +18,8 @@ function Skills() {
             </ul>
           </div>
           <div className="skills-backend">
-            <img src="/programming.svg" className="svg" />
-            <ul>
+            <img src="/programming.svg" className={styles.svg} />
+            <ul className={styles.list}>
               <li>Node</li>
               <li>Express</li>
               <li>MongoDB</li>
@@ -27,8 +28,8 @@ function Skills() {
             </ul>
           </div>
           <div className="skills-tools">
-            <img src="/technology.svg" className="svg" />
-            <ul>
+            <img src="/technology.svg" className={styles.svg} />
+            <ul className={styles.list}>
               <li>VS Code</li>
               <li>NPM</li>
               <li>Git & Github</li>
@@ -37,8 +38,8 @@ function Skills() {
             </ul>
           </div>
           <div className="skills-tolearn">
-            <img src="/interests.svg" className="svg" />
-            <ul>
+            <img src="/interests.svg" className={styles.svg} />
+            <ul className={styles.list}>
               <li>AWS</li>
               <li>TypeScript</li>
               <li>Python</li>
